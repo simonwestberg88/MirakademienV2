4289ddf750a2c08a78c62e1db5b02135907030ba
simon <simon.westberg88@gmail.com>
2024-08-28 13:06:39 +0200

    welcome done

--- mir-web-v2/app/page.tsx
+++ mir-web-v2/app/page.tsx
@@ -3,11 +3,13 @@ import styles from "./page.module.css";
 import { Box } from "@mui/material";
 import Footer from "@/components/footer";
 import HomeDei from "@/components/home/dei";
+import HomeWelcome from "@/components/home/welcome";
 
 export default function Home() {
   return (
     <Box sx={{ backgroundColor: "rgba(237, 230, 212, 0.5)" }}>
     <HomeDei />
+    <HomeWelcome />
     <Box>
       <Footer></Footer>
     </Box>
