cd06694a26d8e94922ab19077dac40e3111e0cdb
simon <simon.westberg88@gmail.com>
2024-08-28 11:51:44 +0200

    images. set base containers for the home page

--- mir-web-v2/app/page.tsx
+++ mir-web-v2/app/page.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { Box } from "@mui/material";
 import Footer from "@/components/footer";
+import HomeDei from "@/components/home/dei";
 
 export default function Home() {
   return (
     <Box>
-    <img src="/mir_logo.svg" alt="MIR Logo" />
+    <HomeDei />
     <Box>
       <Footer></Footer>
     </Box>
---
+++ mir-web-v2/components/home/dei.tsx
@@ -0,0 +1,21 @@
+"use client"
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { useTheme } from '@mui/material/styles';
+
+
+export default function HomeDei({ text = "Contained", ...props }) {
+    const theme = useTheme();
+    return (
+        <Button variant="contained"
+            sx={{
+                borderRadius: '30px',
+                color: theme.palette.background.default,
+                background: "#199DEA",
+                fontSize: theme.typography.body1.fontSize,
+                lineHeight: theme.typography.body1.lineHeight,
+                fontFamily: theme.typography.body1.fontFamily,
+                textTransform: "none"
+            }}>{text}</Button>
+    );
+}
