100e6a9767dd3e83b99d9be6fd9844a5f013499d
simon <simon.westberg88@gmail.com>
2024-08-28 09:22:46 +0200

    desktop menu

--- mir-web-v2/components/app-bar.tsx
+++ mir-web-v2/components/app-bar.tsx
@@ -9,10 +9,12 @@ import MenuIcon from '@mui/icons-material/Menu';
 import BlueButton from './blue_button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
 
 export default function ButtonAppBar() {
   const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleMenuClick = (event) => {
@@ -25,9 +27,11 @@ export default function ButtonAppBar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ height: 83, backgroundColor: theme.palette.background.default }}>
-        <Toolbar sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', height: '100%' }}>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+  <AppBar position="static" sx={{ height: 83, backgroundColor: theme.palette.background.default }}>
+    <Toolbar sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', height: '100%' }}>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        {isMobile ? (
+          <>
             <IconButton
               size="large"
               edge="start"
@@ -39,61 +43,96 @@ export default function ButtonAppBar() {
               <MenuIcon />
             </IconButton>
             <img src="/mir_logo.svg" width={121.85} height={61.25} alt="MIR Logo" />
-          </Box>
-          <BlueButton text="Learn More" />
-        </Toolbar>
-      </AppBar>
-
-      <Menu
-        anchorEl={anchorEl}
-        open={Boolean(anchorEl)}
-        onClose={handleMenuClose}
-        PaperProps={{
-          sx: {
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            margin: 0, // Remove any margin
-            padding: 0, // Remove any padding
-            backgroundColor: theme.palette.background.default,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'flex-start', // Align items to the top
-            alignItems: 'flex-start', // Align items to the left
-          },
-        }}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-      >
-        <Box sx={{ display: 'flex', alignItems: 'center', width: '100%', padding: theme.spacing(2) }}>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ color: "black", marginRight: theme.spacing(2) }}
-            onClick={handleMenuClose} // Optionally, close the menu when clicking the icon again
-          >
-            <MenuIcon />
-          </IconButton>
+          </>
+        ) : (
           <img src="/mir_logo.svg" width={121.85} height={61.25} alt="MIR Logo" />
+        )}
+      </Box>
+
+      {isMobile ? (
+        <BlueButton text="Learn More" />
+      ) : (
+        <Box
+          sx={{
+            display: 'flex',
+            flexGrow: 1, // Make this Box take up remaining space
+            gap: 2,
+            alignItems: 'center',
+            '& > *': {
+              fontSize: theme.typography.body1.fontSize,
+              color: theme.palette.text.primary, // Ensure black text color
+              textAlign: 'left', // Left-align text
+            },
+          }}
+        >
+          <MenuItem component="a" href="#our-story">Our Story</MenuItem>
+          <MenuItem component="a" href="#services">Services</MenuItem>
+          <MenuItem component="a" href="#projects">Projects</MenuItem>
+          <MenuItem component="a" href="#news">News</MenuItem>
+          <MenuItem component="a" href="#blog">Blog</MenuItem>
+          <MenuItem component="a" href="#reports">Reports</MenuItem>
+          <MenuItem component="a" href="#contacts">Contacts</MenuItem>
         </Box>
-        <MenuItem onClick={handleMenuClose} sx={{ fontSize: '24px', width: '100%' }}>Our Story</MenuItem>
-        <MenuItem onClick={handleMenuClose} sx={{ fontSize: '24px', width: '100%' }}>Services</MenuItem>
-        <MenuItem onClick={handleMenuClose} sx={{ fontSize: '24px', width: '100%' }}>Projects</MenuItem>
-        <MenuItem onClick={handleMenuClose} sx={{ fontSize: '24px', width: '100%' }}>News</MenuItem>
-        <MenuItem onClick={handleMenuClose} sx={{ fontSize: '24px', width: '100%' }}>Blog</MenuItem>
-        <MenuItem onClick={handleMenuClose} sx={{ fontSize: '24px', width: '100%' }}>Reports</MenuItem>
-        <MenuItem onClick={handleMenuClose} sx={{ fontSize: '24px', width: '100%' }}>Contacts</MenuItem>
-      </Menu>
+      )}
+
+      <Box sx={{ display: isMobile ? 'none' : 'flex', alignItems: 'center' }}>
+        <BlueButton text="Contact Us" />
+      </Box>
+    </Toolbar>
+  </AppBar>
+
+      {isMobile && (
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+          PaperProps={{
+            sx: {
+              position: 'fixed',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              margin: 0, // Remove any margin
+              padding: 0, // Remove any padding
+              backgroundColor: theme.palette.background.default,
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'flex-start', // Align items to the top
+              alignItems: 'flex-start', // Align items to the left
+            },
+          }}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+        >
+          <Box sx={{ display: 'flex', alignItems: 'center', width: '100%', padding: theme.spacing(2) }}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ color: "black", marginRight: theme.spacing(2) }}
+              onClick={handleMenuClose} // Optionally, close the menu when clicking the icon again
+            >
+              <MenuIcon />
+            </IconButton>
+            <img src="/mir_logo.svg" width={121.85} height={61.25} alt="MIR Logo" />
+          </Box>
+          <MenuItem onClick={handleMenuClose} sx={{ fontSize: '24px', width: '100%' }}>Our Story</MenuItem>
+          <MenuItem onClick={handleMenuClose} sx={{ fontSize: '24px', width: '100%' }}>Services</MenuItem>
+          <MenuItem onClick={handleMenuClose} sx={{ fontSize: '24px', width: '100%' }}>Projects</MenuItem>
+          <MenuItem onClick={handleMenuClose} sx={{ fontSize: '24px', width: '100%' }}>News</MenuItem>
+          <MenuItem onClick={handleMenuClose} sx={{ fontSize: '24px', width: '100%' }}>Blog</MenuItem>
+          <MenuItem onClick={handleMenuClose} sx={{ fontSize: '24px', width: '100%' }}>Reports</MenuItem>
+          <MenuItem onClick={handleMenuClose} sx={{ fontSize: '24px', width: '100%' }}>Contacts</MenuItem>
+        </Menu>
+      )}
     </Box>
   );
 }
