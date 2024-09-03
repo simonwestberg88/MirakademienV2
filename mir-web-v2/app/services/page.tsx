026be7c7ed30bde20c8782ad64257a1677abc714
simon <simon.westberg88@gmail.com>
2024-09-03 12:29:44 +0200

    dei123

--- mir-web-v2/app/services/page.tsx
+++ mir-web-v2/app/services/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import { Box } from "@mui/material"; import Footer from "../components/footer";
 import CoverComponent from "../components/cover";
+import Dei123 from "./dei123";
 
 export default function Services() {
     return (
         <Box>
             <CoverComponent top="Our Services" heading="Unlock Your Potential" buttonText="Contact Us" imgSrc="/services/cover.png" />
+            <Dei123 />
             <Footer />
         </Box>
     );
