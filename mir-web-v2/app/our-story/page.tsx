eb432165656d1f1ae15af0dc01c2ba89a34b54ef
simon <simon.westberg88@gmail.com>
2024-09-03 10:14:25 +0200

    timeline done

--- mir-web-v2/app/our-story/page.tsx
+++ mir-web-v2/app/our-story/page.tsx
@@ -3,12 +3,14 @@
 import { useTheme } from "@emotion/react";
 import { Box, Typography } from "@mui/material";
 import OurStoryStart from "./componenets/first";
+import OurStoryTimeline from "./componenets/timeline";
 
 export default function OurStoryPage() {
   const theme = useTheme();
   return (
     <Box>
       <OurStoryStart />
+      <OurStoryTimeline />
     </Box>
   );
 }
