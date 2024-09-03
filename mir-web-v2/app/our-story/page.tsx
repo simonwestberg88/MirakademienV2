f0e1acd0b41cd576cf6fef31f29e2ca0b409f6e9
simon <simon.westberg88@gmail.com>
2024-09-03 11:50:44 +0200

    our story done

--- mir-web-v2/app/our-story/page.tsx
+++ mir-web-v2/app/our-story/page.tsx
@@ -1,16 +1,48 @@
 "use client"
 
 import { useTheme } from "@emotion/react";
-import { Box, Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 import OurStoryStart from "./componenets/first";
 import OurStoryTimeline from "./componenets/timeline";
 import OurStoryEthos from "./componenets/ethos";
 import Footer from "../components/footer";
+import Image from "next/image";
+import BlueButton from "../components/blue_button";
 
 export default function OurStoryPage() {
   const theme = useTheme();
   return (
     <Box>
+      <Box sx={{ ml: "64px", display: "flex", gap: "32px", mt: "24px", mb: "24px" }}>
+        <Link sx={{ fontWeight: 500 }}>Home</Link>
+        <Typography>Our Story</Typography>
+      </Box>
+      <Box
+        sx={{
+          ml: "64px",
+          mr: "64px",
+          height: "350px", // Set the height of the Box
+        }}
+      >
+        <Box
+          sx={{
+            borderRadius: "30px",
+            width: '100%', // Set the width of the Box
+            height: "100%", // Set the height of the Box
+            backgroundImage: 'url(/our-story/cover.jpg)', // Set the background image
+            backgroundSize: 'cover', // Make sure the image covers the entire box
+            backgroundPosition: 'center', // Center the image in the box
+            backgroundRepeat: 'no-repeat', // Prevent the image from repeating
+          }}
+        >
+          <Box sx={{display:"flex", flexDirection:"column", alignItems:"center", pt: "39px", gap: "24px"}}>
+            <Typography color="white" sx={{fontWeight: 600}}>Our story</Typography>
+            <Typography variant="h2" color="white">Mir Akademien's story</Typography>
+            <BlueButton text="Contact us"></BlueButton>
+            </Box>
+        </Box>
+      </Box>
+
       <OurStoryStart />
       <OurStoryTimeline />
       <OurStoryEthos />
