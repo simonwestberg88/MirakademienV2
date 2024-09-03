877d32e3deed49f3e0563b748fbb933c7cecdaf6
simon <simon.westberg88@gmail.com>
2024-09-03 12:45:49 +0200

    services on-site

--- mir-web-v2/app/services/page.tsx
+++ mir-web-v2/app/services/page.tsx
@@ -2,12 +2,14 @@
 import { Box } from "@mui/material"; import Footer from "../components/footer";
 import CoverComponent from "../components/cover";
 import Services123 from "./one-two-three";
+import ServiceOnsite from "./on-site";
 
 export default function Services() {
     return (
         <Box>
             <CoverComponent top="Our Services" heading="Unlock Your Potential" buttonText="Contact Us" imgSrc="/services/cover.png" />
             <Services123 />
+            <ServiceOnsite />
             <Footer />
         </Box>
     );
