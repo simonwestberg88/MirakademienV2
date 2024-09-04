a90b20f6e4d6b02734a515cc5d14b7111a5c9c18
simon <simon.westberg88@gmail.com>
2024-09-04 10:14:20 +0200

    project type

---
+++ mir-web-v2/app/types/project.ts
@@ -0,0 +1,17 @@
+import { Asset, EntrySkeletonType } from 'contentful';
+import { Document } from '@contentful/rich-text-types';
+
+export interface Project extends EntrySkeletonType  {
+    title: string;
+    cover: Asset;
+    description: string;
+    tags: string[];
+    slug: string;
+    pictures: Asset[];
+    website?: string;
+    linkedIn?:string;
+    facebook?:string;
+    projectDescription: Document;
+    research: string;
+    descriptionPicture: Asset;
+}
