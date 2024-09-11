be42be19b930357e59ede01038fff90c262fbf4a
simon <simon.westberg88@gmail.com>
2024-09-11 10:59:14 +0200

    contact

---
+++ mir-web-v2/app/contacts/cover.tsx
@@ -0,0 +1,19 @@
+import { Box, Link, Typography } from "@mui/material";
+
+export default function ContactsCover() {
+    return (
+        <Box sx={{ ml: "64px", mr: "64px" }}>
+            <Box sx={{ display: "flex", gap: "32px", mt: "24px", mb: "24px" }}>
+                <Link href="/" underline="none" sx={{ fontWeight: 500 }}>Home</Link>
+                <Typography>Contacts</Typography>
+            </Box>
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Box sx={{flex: 1}}>
+                    <Typography variant="h1">Connect with Us</Typography>
+                </Box>
+                <Box sx={{flex: 1}}>
+                    <Typography>We would love to hear from you. Contact us today to learn more about our inclusive education programs and how we can partner together to create a more equitable world.</Typography>
+                </Box>
+            </Box>
+        </Box>);
+}
---
+++ mir-web-v2/app/contacts/page.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import ContactsCover from "./cover";
+
+export default function Contacts(){
+    return(
+        <Box>
+            <ContactsCover />
+        </Box>
+    )
+}
