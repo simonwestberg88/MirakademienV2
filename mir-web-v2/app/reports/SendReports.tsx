670697cbd86ce310cf70a66769eab86b7fbef90d
simon <simon.westberg88@gmail.com>
2024-09-18 09:34:25 +0200

    send reports done

---
+++ mir-web-v2/app/reports/SendReports.tsx
@@ -0,0 +1,38 @@
+"use client"
+import theme from "@/theme";
+import { Box, Typography, TextField, Button } from "@mui/material";
+
+export default function ReportsSend() {
+
+    return (
+        <Box sx={{ pt: "112px", maxWidth: "768px", display: "flex", flexDirection: "column", gap: "16px" }}>
+            <Typography variant="h5">Select the reports you are interested in, enter your email, and we will send them to your email address.</Typography>
+            <Box sx={{ display: "flex", flexDirection: "row", gap: "16px" }}>
+                <TextField
+                    placeholder="Your email here"
+                    sx={{
+                        backgroundColor: "#EDE6D4",
+                        borderRadius: "30px",
+                        "& .MuiOutlinedInput-root": {
+                            borderRadius: "30px",
+                            height: "48px"
+                        },
+                        height: "48px"
+                    }}
+                    variant="outlined"
+                />
+                <Button variant="contained"
+                    sx={{
+                        borderRadius: '30px',
+                        color: theme.palette.background.default,
+                        background: "#199DEA",
+                        fontSize: theme.typography.body1.fontSize,
+                        lineHeight: theme.typography.body1.lineHeight,
+                        fontFamily: theme.typography.body1.fontFamily,
+                        textTransform: "none",
+                        height: "48px"
+                    }}>Send me reports</Button>
+            </Box>
+        </Box>
+    );
+}
--- mir-web-v2/app/reports/page.tsx
+++ mir-web-v2/app/reports/page.tsx
@@ -1,8 +1,10 @@
-import { Box, Link, Typography } from "@mui/material";
+import { Box, Button, Link, TextField, Typography, useTheme } from "@mui/material";
 import ReportCard from "../components/cards/reportCard";
 import { ContentfulClient } from "../lib/client";
 import { Report } from "../types/report";
 import Grid from '@mui/material/Grid2';
+import Footer from "../components/footer";
+import ReportsSend from "./SendReports";
 
 export default async function Reports() {
     const contentfulClient = ContentfulClient.getClient();
@@ -20,44 +22,50 @@ export default async function Reports() {
     } as Report));
 
     return (
-        <Box sx={{ ml: "64px", mr: "64px" }}>
-            <Box sx={{ display: "flex", gap: "32px", mt: "24px", mb: "24px" }}>
-                <Link href="/" underline="none" sx={{ fontWeight: 500 }}>Home</Link>
-                <Typography>Reports</Typography>
-            </Box>
-            <Box
-                sx={{
-                    width: '100%',
-                    height: "398px",
-                    backgroundImage: "url(/reports/cover.png)",
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                    backgroundRepeat: 'no-repeat',
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    ml: "-64px",
-                    pr: "128px"
-                }}
-            >
-                <Box key="b2" sx={{
-                    display: "flex", flexDirection: "column", pt: "39px", gap: "24px", ml: "64px",
-                    mr: "64px",
-                    maxWidth: "768px"
-                }}>
-                    <Typography color="white">Reports</Typography>
-                    <Typography variant="h1" color="white">Here you can access MIR Akademien free reports</Typography>
+        <Box>
+
+            <Box sx={{ ml: "64px", mr: "64px" }}>
+                <Box sx={{ display: "flex", gap: "32px", mt: "24px", mb: "24px" }}>
+                    <Link href="/" underline="none" sx={{ fontWeight: 500 }}>Home</Link>
+                    <Typography>Reports</Typography>
                 </Box>
+                <Box
+                    sx={{
+                        width: '100%',
+                        height: "398px",
+                        backgroundImage: "url(/reports/cover.png)",
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        ml: "-64px",
+                        pr: "128px"
+                    }}
+                >
+                    <Box key="b2" sx={{
+                        display: "flex", flexDirection: "column", pt: "39px", gap: "24px", ml: "64px",
+                        mr: "64px",
+                        maxWidth: "768px"
+                    }}>
+                        <Typography color="white">Reports</Typography>
+                        <Typography variant="h1" color="white">Here you can access MIR Akademien free reports</Typography>
+                    </Box>
+                </Box>
+                <Box sx={{ pt: "112px", flexGrow: 1 }}>
+                    <Grid container spacing={4}>
+                        {reports.map((report) => (
+                            <Grid size={6} key={report.title}>
+                                <ReportCard report={report} />
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Box>
+                <ReportsSend />
             </Box>
-            <Box sx={{ pt: "112px", flexGrow: 1 }}>
-                <Grid container spacing={4}>
-                    {reports.map((report) => (
-                        <Grid size={6} key={report.title}>
-                            <ReportCard report={report} />
-                        </Grid>
-                    ))}
-                </Grid>
-            </Box>
+            <Footer />
         </Box>
+
     )
 }
