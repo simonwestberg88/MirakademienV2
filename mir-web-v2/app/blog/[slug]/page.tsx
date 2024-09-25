df8de423d15cb5109766cdde8b1e77b5a96ee739
simon <simon.westberg88@gmail.com>
2024-09-25 13:15:53 +0200

    almost done with blogs

--- mir-web-v2/app/blog/[slug]/page.tsx
+++ mir-web-v2/app/blog/[slug]/page.tsx
@@ -9,6 +9,8 @@ import XIcon from '@mui/icons-material/X';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { BLOCKS } from '@contentful/rich-text-types';
+import MirButton from '@/app/components/mir-button';
+import Footer from '@/app/components/footer';
 
 export default async function BlogPostPage({ params }: { params: { slug: string } }) {
     const contentfulClient = ContentfulClient.getClient();
@@ -62,54 +64,74 @@ export default async function BlogPostPage({ params }: { params: { slug: string 
     };
 
     return (
-        <Box sx={{ ml: "64px", mr: "64px" }}>
-            <NavMenu link1='Blog' link2={post.title} href1='/blog'></NavMenu>
-            <Box display={"flex"} flexDirection={"column"} gap={"80px"}>
-                <Box display={"flex"} flexDirection={"column"} gap={"16px"}>
-                    <Box display={"fleX"} flexDirection={"row"} gap={"16px"} alignItems="center">
-                        <Typography borderRadius={"20px"} padding={"4px"} pl={"10px"} pr={"10px"} bgcolor={"orange.main"} color={"white"} variant='caption'>{post.category}</Typography>
-                        <Typography variant='caption'>{post.timeToRead} read</Typography>
-                    </Box>
-                    <Typography variant="h1" sx={{ mb: 4 }}>{post.title}</Typography>
-                </Box>
-                <Box display={"flex"} flexDirection={"column"} gap={"32px"}>
-                    <Box
-                        overflow={"hidden"}
-                        position={"relative"}
-                        borderRadius={"40px"}
-                        height="600px"
-                    >
-                        <Image
-                            src={`https:${post.coverImage.fields.file.url}`}
-                            alt="cover image"
-                            layout="fill"
-                            objectFit="cover"
-                        />
-                    </Box>
-                    <Box display="flex" flexDirection="row" justifyContent="space-between" width="100%">
-                        <Box display="flex" flexDirection="row" gap={4}>
-                            <Box display="flex" flexDirection="column">
-                                <Typography>Written by</Typography>
-                                <Typography fontWeight={500}>{post.author}</Typography>
-                            </Box>
+        <Box>
 
-                            <Box display="flex" flexDirection="column">
-                                <Typography>Published on</Typography>
-                                <Typography fontWeight={500}>{post.date}</Typography>
+            <Box sx={{ ml: "64px", mr: "64px" }}>
+                <NavMenu link1='Blog' link2={post.title} href1='/blog'></NavMenu>
+                <Box display={"flex"} flexDirection={"column"} gap={"80px"}>
+                    <Box display={"flex"} flexDirection={"column"} gap={"16px"}>
+                        <Box display={"fleX"} flexDirection={"row"} gap={"16px"} alignItems="center">
+                            <Typography borderRadius={"20px"} padding={"4px"} pl={"10px"} pr={"10px"} bgcolor={"orange.main"} color={"white"} variant='caption'>{post.category}</Typography>
+                            <Typography variant='caption'>{post.timeToRead} read</Typography>
+                        </Box>
+                        <Typography variant="h1" sx={{ mb: 4 }}>{post.title}</Typography>
+                    </Box>
+                    <Box display={"flex"} flexDirection={"column"} gap={"32px"}>
+                        <Box
+                            overflow={"hidden"}
+                            position={"relative"}
+                            borderRadius={"40px"}
+                            height="600px"
+                        >
+                            <Image
+                                src={`https:${post.coverImage.fields.file.url}`}
+                                alt="cover image"
+                                layout="fill"
+                                objectFit="cover"
+                            />
+                        </Box>
+                        <Box display="flex" flexDirection="row" justifyContent="space-between" width="100%">
+                            <Box display="flex" flexDirection="row" gap={4}>
+                                <Box display="flex" flexDirection="column">
+                                    <Typography>Written by</Typography>
+                                    <Typography fontWeight={500}>{post.author}</Typography>
+                                </Box>
+
+                                <Box display="flex" flexDirection="column">
+                                    <Typography>Published on</Typography>
+                                    <Typography fontWeight={500}>{post.date}</Typography>
+                                </Box>
+                            </Box>
+                            <Box display="flex" gap={1}>
+                                <LinkIcon />
+                                <LinkedInIcon />
+                                <XIcon />
+                                <FacebookIcon />
                             </Box>
                         </Box>
-                        <Box display="flex" gap={1}>
-                            <LinkIcon />
-                            <LinkedInIcon />
-                            <XIcon />
-                            <FacebookIcon />
+                    </Box>
+                </Box>
+                <Box maxWidth={"768px"} pt={"112px"} mx="auto">
+                    {documentToReactComponents(post.post, renderOptions)}
+                    <Box key="btmBox" display={"flex"} flexDirection={"column"} gap={"48px"} alignItems="center">
+                        <Box display={"flex"} flexDirection={"column"} gap={"16px"} alignItems="center">
+                            <Typography fontWeight={500}>Share this post</Typography>
+                            <Box alignItems="center">
+                                <LinkIcon />
+                                <LinkedInIcon />
+                                <FacebookIcon />
+                            </Box>
+                            <Box alignItems="center">
+                                {post.tags.map(tag => (
+                                    <MirButton>{tag}</MirButton>
+                                ))}
+                            </Box>
                         </Box>
                     </Box>
                 </Box>
             </Box>
-            <Box maxWidth={"768px"} pt={"112px"} mx="auto">
-                {documentToReactComponents(post.post, renderOptions)}
-            </Box>
+            <Footer />
         </Box>
+
     );
 }
