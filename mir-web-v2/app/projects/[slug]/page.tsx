a556ef2cbd8638764066b5cc592e4f916759a5d1
simon <simon.westberg88@gmail.com>
2024-10-02 18:48:32 +0200

    project page pretty much done

--- mir-web-v2/app/projects/[slug]/page.tsx
+++ mir-web-v2/app/projects/[slug]/page.tsx
@@ -68,15 +68,15 @@ export default async function ProjectPage({ params }: { params: { slug: string }
                             <Typography>{project.description}</Typography>
                         </Box>
                     </Box>
-                    <Box key={"picureBox"} display="flex" flexDirection="column" gap="16px">
-                        <Box display="flex" flexDirection={"row"} gap={"16px"} ml={"-400px"} overflow="visible" width={2732}>
+                    <Box key={"picureBox"} display="flex" flexDirection="column" gap="16px" overflow="hidden" ml="-64px" mr="-164px">
+                        <Box display="flex" flexDirection={"row"} gap={"16px"} ml={"-400px"}  width={2732}>
                             <HeaderBox color="gray" alt="pic1" picture={project.headerPicsRow1?.[0]} />
                             <HeaderBox color='#e9f6f7' title={project.headerTitle1} text={project.headerText1} />
                             <HeaderBox color="gray" alt="pic2" picture={project.headerPicsRow1?.[1]} />
                             <HeaderBox color="gray" alt="pic3" picture={project.headerPicsRow1?.[2]} />
                             <HeaderBox color="red" />
                         </Box>
-                        <Box display="flex" flexDirection={"row"} gap={"16px"} ml={"-100px"} overflow="visible" width={2732}>
+                        <Box display="flex" flexDirection={"row"} gap={"16px"} ml={"-100px"}  width={2732}>
                             <HeaderBox color="gray" alt="pic1" picture={project.headerPicsRow2?.[0]} />
                             <HeaderBox color="gray" alt="pic2" picture={project.headerPicsRow2?.[1]} />
                             <HeaderBox color='#f3b27c' title={project.headerTitle2} text={project.headerText2} />
