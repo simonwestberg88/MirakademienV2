46631d6d569d5bb46a2206c0f4210764c73e79bf
simon <simon.westberg88@gmail.com>
2024-10-03 09:29:36 +0200

    fix blog card

--- mir-web-v2/app/components/home/cards/blog_card.tsx
+++ mir-web-v2/app/components/home/cards/blog_card.tsx
@@ -10,11 +10,10 @@ export default function BlogCard({ post }: BlogCardProps) {
     const imageUrl = post.coverImage.fields.file?.url as string;
     return (
         <Box display="flex" flexDirection="column" width="405px" gap="24px">
-            <Box width= "405px" height="205px" borderRadius="40px" overflow="hidden">
+            <Box width= "405px" height="205px" borderRadius="40px" overflow="hidden" position="relative">
                 <Image
                     src={`https:${imageUrl}`} alt={post.contentTypeId}
-                    width={405}
-                    height={240}
+                    layout='fill'
                 ></Image>
             </Box>
             <Box>
