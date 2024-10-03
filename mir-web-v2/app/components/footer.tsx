9019cec22d9b3f4628ea92ce471bcbb3d089b208
simon <simon.westberg88@gmail.com>
2024-10-03 13:23:56 +0200

    sum more footer stuff

--- mir-web-v2/app/components/footer.tsx
+++ mir-web-v2/app/components/footer.tsx
@@ -6,9 +6,24 @@ import { Box, Button, Divider, Link, TextField, Typography } from '@mui/material
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import Image from 'next/image';
+import MirButton from './mir-button';
+import { useState } from 'react';
 
 export default function Footer() {
     const theme = useTheme();
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+
+        // HTML5 validation automatically handles email format validation
+        if (!email) {
+            console.log("Email is required");
+        } else {
+            console.log("Subscribed with email:", email);
+            // Add any additional submission logic here
+        }
+    };
     return (
         <Box key="l1" sx={{ height: 545, width: "100%", backgroundColor: theme.palette.background.default, mt: "112px" }}>
             <Box key="l2_1" sx={{ paddingTop: "80px", pl: "64px", pr: "64px", display: "flex", justifyContent: "space-between", alignItems: "top" }}>
@@ -17,18 +32,32 @@ export default function Footer() {
                     <Box key="l4_1">
                         <Typography sx={{ fontWeight: "bold" }}>About Us</Typography>
                         <Box sx={{ pt: "20px", display: "flex", flexDirection: "column", gap: "16px" }}>
-                            <Typography>Our Story</Typography>
-                            <Typography>Services</Typography>
-                            <Typography>Projects</Typography>
-                            <Typography>Contacts</Typography>
+                            <Link underline='none' href="/our-story">
+                                <Typography>Our Story</Typography>
+                            </Link>
+                            <Link underline='none' href="/services">
+                                <Typography>Services</Typography>
+                            </Link>
+                            <Link underline='none' href="/projects">
+                                <Typography>Projects</Typography>
+                            </Link>
+                            <Link underline='none' href="/contacts">
+                                <Typography>Contacts</Typography>
+                            </Link>
                         </Box>
                     </Box>
                     <Box key="l4_2">
                         <Typography sx={{ fontWeight: "bold" }}>Resources</Typography>
                         <Box sx={{ pt: "20px", display: "flex", flexDirection: "column", gap: "16px" }}>
-                            <Typography>News</Typography>
-                            <Typography>Blog</Typography>
-                            <Typography>Reports</Typography>
+                            <Link underline='none' href="/news">
+                                <Typography>News</Typography>
+                            </Link>
+                            <Link underline='none' href="/blog">
+                                <Typography>Blog</Typography>
+                            </Link>
+                            <Link underline='none' href="/reports">
+                                <Typography>Reports</Typography>
+                            </Link>
                         </Box>
                     </Box>
                 </Box>
@@ -36,29 +65,37 @@ export default function Footer() {
                     <Box sx={{ display: "flex", flexDirection: "column", gap: "16px" }}>
                         <Typography sx={{ fontWeight: "bold" }}>Subscribe</Typography>
                         <Typography>Join our newsletter to stay up to date on features and releases.</Typography>
-                        <Box sx={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
-                            <TextField
-                                placeholder="Enter your email"
-                                sx={{
-                                    backgroundColor: "white",
-                                    borderRadius: "40px",
-                                    "& .MuiOutlinedInput-root": {
+                        <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+                            <Box sx={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+                                <TextField
+                                    placeholder="Enter your email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    type="email" // Built-in HTML5 validation for email format
+                                    required // Ensures that the field is mandatory
+                                    sx={{
+                                        backgroundColor: "white",
                                         borderRadius: "40px",
-                                    },
-                                    flexGrow: 1,
-                                    mr: 2,
-                                    "& .MuiInputBase-input": {
-                                        color: theme.palette.text.primary,
-                                    },
-                                }}
-                                variant="outlined"
-                            />
-                            <Button variant="outlined"
-                                sx={{ borderRadius: 40, borderColor: theme.palette.info.main, color: theme.palette.info.main, textTransform: "none", ...theme.typography.body1 }}
-                            >
-                                Subscribe
-                            </Button>
-                        </Box>
+                                        "& .MuiOutlinedInput-root": {
+                                            borderRadius: "40px",
+                                        },
+                                        flexGrow: 1,
+                                        mr: 2,
+                                        "& .MuiInputBase-input": {
+                                            color: "black",
+                                        },
+                                    }}
+                                    variant="outlined"
+                                />
+                                <MirButton
+                                    type="submit"
+                                    variant="outlined"
+                                    color="info"
+                                >
+                                    Subscribe
+                                </MirButton>
+                            </Box>
+                        </form>
                         <Typography variant="caption">By subscribing you agree to with our Privacy Policy and provide consent to receive updates from our company.</Typography>
                     </Box>
                 </Box>
