91167f02d5cca4509b0df1c83805cb4506e1eb59
simon <simon.westberg88@gmail.com>
2024-10-09 11:08:59 +0200

    home team

--- mir-web-v2/app/components/home/team.tsx
+++ mir-web-v2/app/components/home/team.tsx
@@ -1,25 +1,23 @@
+"use client"
 import { TeamMember } from '@/app/types/TeamMember';
-import { Box, Typography } from '@mui/material';
-import TeamCard from './cards/team_card';
+import { Box, useMediaQuery, useTheme } from '@mui/material';
+import TeamDesktop from './team/desktop';
+import TeamMobile from './team/mobile';
 
 interface HomeTeamProps {
     teamMembers: TeamMember[];
 }
 
 export default function HomeTeam({ teamMembers }: HomeTeamProps) {
+    const theme = useTheme();
+    const isDesktop = useMediaQuery(theme.breakpoints.up('sm'));
+
     return (
-        <Box sx={{ pt: '112px', px: '64px', textAlign: 'center' }}>
-            <Typography variant="h2" gutterBottom>
-                Meet Our Team
-            </Typography>
-            <Typography variant="subtitle1" gutterBottom>
-                Get to know the talented individuals behind MIR Akademien.
-            </Typography>
-            <Box sx={{display: "flex", flexWrap: "wrap", rowGap: "96px", justifyContent: "center", pt: "96px"}}>
-                {teamMembers.map((member) => (
-                    <TeamCard key={member.name} member={member} />
-                ))}
-            </Box>
+        <Box>
+            {isDesktop ? (<TeamDesktop teamMembers={teamMembers} />) : (
+                <TeamMobile teamMembers={teamMembers} />
+            )}
         </Box>
+
     );
 }
---
+++ mir-web-v2/app/components/home/team/desktop.tsx
@@ -0,0 +1,25 @@
+import { TeamMember } from '@/app/types/TeamMember';
+import { Box, Typography } from '@mui/material';
+import TeamCard from '../cards/team_card';
+
+interface HomeTeamProps {
+    teamMembers: TeamMember[];
+}
+
+export default function TeamDesktop({ teamMembers }: HomeTeamProps) {
+    return (
+        <Box sx={{ pt: '112px', px: '64px', textAlign: 'center' }}>
+            <Typography variant="h2" gutterBottom>
+                Meet Our Team
+            </Typography>
+            <Typography variant="subtitle1" gutterBottom>
+                Get to know the talented individuals behind MIR Akademien.
+            </Typography>
+            <Box sx={{display: "flex", flexWrap: "wrap", rowGap: "96px", justifyContent: "center", pt: "96px"}}>
+                {teamMembers.map((member) => (
+                    <TeamCard key={member.name} member={member} />
+                ))}
+            </Box>
+        </Box>
+    );
+}
