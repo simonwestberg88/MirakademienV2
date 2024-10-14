d960d5ead6626ddbdf6ffedd5cf957b70e349e0e
simon <simon.westberg88@gmail.com>
2024-10-14 11:09:01 +0200

    supportblock

--- mir-web-v2/app/projects/[slug]/support/supportBlock.tsx
+++ mir-web-v2/app/projects/[slug]/support/supportBlock.tsx
@@ -1,24 +1,17 @@
-import MirButton from "@/app/components/mir-button";
-import { Box, Typography } from "@mui/material";
+"use client"
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import SupportBlockDesktop from "./desktop";
+import SupportBlockMobile from "./mobile";
 
 export default function SupportBlock() {
+    const theme = useTheme();
+    const isDesktop = useMediaQuery(theme.breakpoints.up('sm'));
+
     return (
-        <Box pt="112px" pb="112px">
-            <Box display="flex" flexDirection="column" width="768" alignItems="center" gap="24px">
-                <Typography variant="h2">Support Our Exciting New Projects</Typography>
-                <Typography>Discover how you can make a difference and contribute to our impactful projects.</Typography>
-                <Box display="flex" gap="16px">
-                    <MirButton
-                        sx={{ width: "160px" }}
-                        variant="contained"
-                    >Get involved</MirButton>
-                    <MirButton
-                        variant="outlined"
-                        color="info"
-                        sx={{ width: "160px" }}
-                    >Learn more</MirButton>
-                </Box>
-            </Box>
+        <Box>
+            {isDesktop ? (<SupportBlockDesktop />) : (
+                <SupportBlockMobile  />
+            )}
         </Box>
-    )
+    );
 }
