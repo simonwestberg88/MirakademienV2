497a07a4d4844c6909f217d648eef7f83d60e4f0
simon <simon.westberg88@gmail.com>
2024-10-15 14:53:03 +0200

    posts

---
+++ mir-web-v2/app/blog/posts/desktop.tsx
@@ -0,0 +1,76 @@
+'use client'
+import Grid from '@mui/material/Grid2';
+import { Blog } from '../../types/Blog';
+import { useState } from 'react';
+import { Box } from '@mui/material';
+import MirButton from '../../components/mir-button';
+import BlogCard from '../../components/cards/blogCard';
+
+
+interface BlogPostsProps {
+    posts: Blog[];
+    categories: string[];
+}
+export default function PostsDesktop(props: BlogPostsProps) {
+    const [showAllBlogPosts, setShowAllBlogPosts] = useState(false);
+    const [filterCategory, setFilterCategory] = useState<string>("all");
+
+    const filteredBlogPosts = filterCategory === "all"
+        ? props.posts
+        : props.posts.filter(post => post.category === filterCategory);
+
+    const visibleBlogPosts = showAllBlogPosts ? filteredBlogPosts : filteredBlogPosts.slice(0, 6);
+
+    const togglePosts = () => {
+        setShowAllBlogPosts(!showAllBlogPosts);
+    };
+
+    const filterCategories = (category: string) => {
+        setFilterCategory(category);
+        setShowAllBlogPosts(false);
+    };
+
+    const sxSelected = { color: "orange.main", borderColor: "orange.main" };
+    const sxNotSelected = { color: "black", borderColor: "orange.main" };
+
+    return (
+        <Box display={"flex"} flexDirection={"column"} gap={"64px"}>
+            <Box display={"flex"} flexDirection={"row"}>
+                <MirButton
+                    onClick={() => filterCategories("all")}
+                    variant={filterCategory === "all" ? "outlined" : "text"}
+                    sx={filterCategory === "all" ? sxSelected : sxNotSelected}
+                >
+                    View all
+                </MirButton>
+                {props.categories.map(category => (
+                    <MirButton
+                        key={category}
+                        sx={filterCategory === category ? sxSelected : sxNotSelected}
+                        onClick={() => filterCategories(category)}
+                        variant={filterCategory === category ? "outlined" : "text"}
+                    >{category}</MirButton>
+                ))}
+            </Box>
+            <Grid container spacing={4}>
+                {visibleBlogPosts.map((post) => (
+                    <Grid size={4} key={post.title}>
+                        <BlogCard post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+            {filteredBlogPosts.length > 6 && (
+                <Box display="flex" justifyContent="center">
+                    <MirButton
+                        onClick={togglePosts}
+                        variant='outlined'
+                        color="info"
+                        sx={{ width: "160px" }}
+                    >
+                        {showAllBlogPosts ? "Show fewer" : "Show more"}
+                    </MirButton>
+                </Box>
+            )}
+        </Box>
+    );
+}
--- mir-web-v2/app/blog/posts/posts.tsx
+++ mir-web-v2/app/blog/posts/posts.tsx
@@ -1,10 +1,8 @@
 'use client'
-import Grid from '@mui/material/Grid2';
 import { Blog } from '../../types/Blog';
-import { useState } from 'react';
-import { Box } from '@mui/material';
-import MirButton from '../../components/mir-button';
-import BlogCard from '../../components/cards/blogCard';
+import { Box, useMediaQuery, useTheme } from '@mui/material';
+import PostsDesktop from './desktop';
+import PostsMobile from './mobile';
 
 
 interface BlogPostsProps {
@@ -12,65 +10,14 @@ interface BlogPostsProps {
     categories: string[];
 }
 export default function Posts(props: BlogPostsProps) {
-    const [showAllBlogPosts, setShowAllBlogPosts] = useState(false);
-    const [filterCategory, setFilterCategory] = useState<string>("all");
-
-    const filteredBlogPosts = filterCategory === "all"
-        ? props.posts
-        : props.posts.filter(post => post.category === filterCategory);
-
-    const visibleBlogPosts = showAllBlogPosts ? filteredBlogPosts : filteredBlogPosts.slice(0, 6);
-
-    const togglePosts = () => {
-        setShowAllBlogPosts(!showAllBlogPosts);
-    };
-
-    const filterCategories = (category: string) => {
-        setFilterCategory(category);
-        setShowAllBlogPosts(false);
-    };
-
-    const sxSelected = { color: "orange.main", borderColor: "orange.main" };
-    const sxNotSelected = { color: "black", borderColor: "orange.main" };
+    const theme = useTheme();
+    const isDesktop = useMediaQuery(theme.breakpoints.up('sm'));
 
     return (
-        <Box display={"flex"} flexDirection={"column"} gap={"64px"}>
-            <Box display={"flex"} flexDirection={"row"}>
-                <MirButton
-                    onClick={() => filterCategories("all")}
-                    variant={filterCategory === "all" ? "outlined" : "text"}
-                    sx={filterCategory === "all" ? sxSelected : sxNotSelected}
-                >
-                    View all
-                </MirButton>
-                {props.categories.map(category => (
-                    <MirButton
-                        key={category}
-                        sx={filterCategory === category ? sxSelected : sxNotSelected}
-                        onClick={() => filterCategories(category)}
-                        variant={filterCategory === category ? "outlined" : "text"}
-                    >{category}</MirButton>
-                ))}
-            </Box>
-            <Grid container spacing={4}>
-                {visibleBlogPosts.map((post) => (
-                    <Grid size={4} key={post.title}>
-                        <BlogCard post={post} />
-                    </Grid>
-                ))}
-            </Grid>
-            {filteredBlogPosts.length > 6 && (
-                <Box display="flex" justifyContent="center">
-                    <MirButton
-                        onClick={togglePosts}
-                        variant='outlined'
-                        color="info"
-                        sx={{ width: "160px" }}
-                    >
-                        {showAllBlogPosts ? "Show fewer" : "Show more"}
-                    </MirButton>
-                </Box>
-            )}
+        <Box>
+            {isDesktop ? (<PostsDesktop categories={props.categories} posts={props.posts} />) : (
+            <PostsMobile categories={props.categories} posts={props.posts} />
+        )}
         </Box>
     );
 }
